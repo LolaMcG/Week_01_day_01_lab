445df771935d513d1c9b031971a3c3b20a4657e8
Laura McGarvey <lauraamcg@gmail.com>
2022-11-28 17:00:42 +0000

    first commit

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,7 @@
+class Team:
+    def __init__ (self, input_name, input_list_of_names, input_coach):
+        self.name = input_name,
+        self.list_of_names = input_list_of_names,
+        self.coach = input_coach
+    
+    # def add_player():
