6f14fc2e05a655f41d9ef2909287670da87c9130
Laura McGarvey <lauraamcg@gmail.com>
2022-11-29 09:38:50 +0000

    homework commits

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -1,7 +1,25 @@
 class Team:
+
     def __init__ (self, input_name, input_list_of_names, input_coach):
-        self.name = input_name,
-        self.list_of_names = input_list_of_names,
+        self.name = input_name
+        self.players = input_list_of_names
         self.coach = input_coach
+        self.points = 0
+        
     
-    # def add_player():
+    def add_player(self, player_name):
+        self.players.append(player_name)
+
+    def has_player(self, new_player):
+        for player in self.players:
+            if player == new_player:
+                return True
+        else:
+            return False
+
+    def team_has_points(self, team_points):
+        self.points(team_points)
+        
+    def play_game(self, result):
+        if result == True:
+            self.points += 3
